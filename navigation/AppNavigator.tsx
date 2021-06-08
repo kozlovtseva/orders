834f5de5abb29142d323560d688e480e7d192cfb
Alina <alina.lm@mail.ru>
2021-06-08 18:41:00 +0300

    add themes

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { HomeScreen } from '../screens';
@@ -12,12 +12,39 @@ export type RootStackParamList = {
 
 const Stack = createStackNavigator<RootStackParamList>();
 
+const Theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#006FC3',
+        text: '#ffffff',
+    },
+};
+
 const AppNavigator = (): React.ReactElement => {
     return (
-        <NavigationContainer>
+        <NavigationContainer theme={Theme}>
             <Stack.Navigator initialRouteName="Мои заказы">
-                <Stack.Screen name="Мои заказы" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen
+                    name="Мои заказы"
+                    component={HomeScreen}
+                    options={{
+                        headerStyle: {
+                            backgroundColor: Theme.colors.primary,
+                        },
+                        headerTintColor: Theme.colors.text,
+                    }}
+                />
+                <Stack.Screen
+                    name="Details"
+                    component={DetailsScreen}
+                    options={{
+                        headerStyle: {
+                            backgroundColor: Theme.colors.primary,
+                        },
+                        headerTintColor: Theme.colors.text,
+                    }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
