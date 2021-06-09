d7a4811aa52e476f1ee7afda77111e43cba4002c
Alina <alina.lm@mail.ru>
2021-06-09 23:35:50 +0300

    refactor OrdersLabel

--- components/Label/OrdersLabel.tsx
+++ components/Label/OrdersLabel.tsx
@@ -1,19 +1,46 @@
-import React, { FC, ReactElement, useContext } from 'react';
+import React, { FC, ReactElement, useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
 import { ThemeContext } from '../../App';
 import { ITheme } from '../../interfaces/theme';
+import { IStatusList, IStatusListItem } from '../../interfaces/orders';
 
 interface IProps {
-    name: string;
-    color: string;
+    status: string;
 }
 
-const OrdersLabel: FC<IProps> = ({ name, color }): ReactElement => {
+const OrdersLabel: FC<IProps> = ({ status }): ReactElement => {
     const theme: ITheme = useContext(ThemeContext);
 
+    const [name, setName] = useState<string>('');
+    const [color, setColor] = useState<string>('');
+
+    useEffect(() => getStatus(status), []);
+
+    const getStatus = (status: string) => {
+        const statusList: IStatusList = {
+            canceled: {
+                name: 'отменен',
+                color: '#EF6366',
+            },
+            done: {
+                name: 'приготовлен',
+                color: '#23DCAD',
+            },
+            sent_to_kitchen: {
+                name: 'отправлено на кухню',
+                color: '#FDA53C',
+            },
+            new: {
+                name: 'новый',
+                color: '#E173D6',
+            },
+        };
+        setName(statusList[status.toLowerCase()].name.toUpperCase());
+        setColor(statusList[status.toLowerCase()].color);
+    };
     return (
         <View style={[styles.container, { backgroundColor: color ? color : theme.colors.primary }]}>
-            <Text style={[styles.text, { color: theme.colors.title }]}>{name.toUpperCase()}</Text>
+            <Text style={[styles.text, { color: theme.colors.title }]}>{name}</Text>
         </View>
     );
 };
