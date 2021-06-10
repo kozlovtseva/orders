4ffb3fc56bbeebc46be29ce22dc35aae403ac3cb
Alina <alina.lm@mail.ru>
2021-06-10 11:54:26 +0300

    use real data

--- store/OrdersStore.ts
+++ store/OrdersStore.ts
@@ -1,4 +1,4 @@
-import { action, computed, makeObservable, observable } from 'mobx';
+import { action, computed, makeObservable, observable, runInAction } from 'mobx';
 
 import config from '../api';
 import { IOrdersDateListItem, IOrdersGroupedListItem, IOrdersListItem } from '../interfaces/orders';
@@ -22,129 +22,15 @@ class OrdersStore {
     groupedList: IOrdersGroupedListItem[] = [];
 
     async init() {
-        // let response = await fetch(`${config.api}/orders`, {
-        //     method: 'GET',
-        // });
-        // this.list = await response.json();
+        let response = await fetch(`${config.api}/orders`, {
+            method: 'GET',
+        });
+        const newList = await response.json();
 
-        let newList = [
-            {
-                id: 47743,
-                title: '# 47743',
-                creationDate: '2021-06-09T16:04:07.390Z',
-                status: 'SENT_TO_KITCHEN',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-            {
-                id: 47744,
-                title: '# 47744',
-                creationDate: '2021-06-08T16:04:07.390Z',
-                status: 'NEW',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-            {
-                id: 47745,
-                title: '# 47745',
-                creationDate: '2021-06-06T16:04:07.390Z',
-                status: 'CANCELED',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-            {
-                id: 47746,
-                title: '# 47746',
-                creationDate: '2021-06-06T16:04:07.390Z',
-                status: 'DONE',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-            {
-                id: 47756,
-                title: '# 47756',
-                creationDate: '2021-06-06T16:04:07.390Z',
-                status: 'DONE',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-            {
-                id: 47766,
-                title: '# 47766',
-                creationDate: '2021-06-06T16:04:07.390Z',
-                status: 'DONE',
-                address: 'ул. Зеленая, д. 19, кв. 40',
-                courierComment:
-                    'Aut eum nulla et doloremque itaque veniam aliquam non occaecati. Omnis dignissimos alias quis aut in aut recusandae harum. In nam quia vero nesciunt totam assumenda.',
-                venue: {
-                    id: 41,
-                    title: 'Кафе «Streamlined scalable database»',
-                },
-                guest: {
-                    id: 51,
-                    firstName: 'Анжелика',
-                    lastName: 'Горшкова',
-                    phone: '(955)180-52-59',
-                },
-            },
-        ];
-        this.list = newList;
-        this.groupedList = this.groupList(newList);
+        runInAction(() => {
+            this.list = newList;
+            this.groupedList = this.groupList(newList);
+        });
     }
 
     groupList = (list: any): IOrdersGroupedListItem[] => {
